042ae442d9247f439725c3c2decef80f946f1f36
lithuwix <loremlisumdol@gmail.com>
2023-03-20 07:00:41 +0700

    -feat HW_5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -11,7 +11,7 @@ type PropsType = {
 
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
     const sidebarClass = s.sidebar
-        + (open ? ' ' + s.open : '')
+        + (open ? '' + s.open : '')
     return (
         <>
             {/*затемнение справа от открытого меню*/}
