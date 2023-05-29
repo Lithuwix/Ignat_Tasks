cd0e44cd49673a0dd1aed431d6a3f76de7ac531c
lithuwix <loremlisumdol@gmail.com>
2023-05-29 22:12:11 +0300

    -fix filter by ages fix in hw_8 -- 02

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -17,7 +17,7 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): any => {
       return state;
     }
     case "check": {
-      return state.filter((el) => el.age > 18);
+      return state.filter((el) => el.age > 18).reverse();
     }
     default:
       return state;
